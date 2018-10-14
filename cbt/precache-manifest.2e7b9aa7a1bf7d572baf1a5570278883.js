225d9d2b0f32bb1989087152e06ac6a0fc810015
Glen Davies <glendaviesnz@gmail.com>
2018-10-14 18:20:13 +1300

    more path fixes

--- cbt/precache-manifest.2e7b9aa7a1bf7d572baf1a5570278883.js
+++ cbt/precache-manifest.2e7b9aa7a1bf7d572baf1a5570278883.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = [
   {
     "revision": "229c360febb4351a89df",
-    "url": "/static/js/runtime~main.229c360f.js"
+    "url": "static/js/runtime~main.229c360f.js"
   },
   {
     "revision": "7af7be87c17b71b2c4b5",
-    "url": "/static/js/main.7af7be87.chunk.js"
+    "url": "static/js/main.7af7be87.chunk.js"
   },
   {
     "revision": "bba1e6987a286ed943cf",
-    "url": "/static/js/1.bba1e698.chunk.js"
+    "url": "static/js/1.bba1e698.chunk.js"
   },
   {
     "revision": "7af7be87c17b71b2c4b5",
-    "url": "/static/css/main.20d85dfd.chunk.css"
+    "url": "static/css/main.20d85dfd.chunk.css"
   },
   {
     "revision": "7e4ae9404ab4bd24cb990e2911185a65",
-    "url": "/index.html"
+    "url": "index.html"
   }
 ];
