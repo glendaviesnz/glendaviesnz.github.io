ae388583a63d7360884d9494e4731a38d6c3f038
Glen Davies <glendaviesnz@gmail.com>
2018-10-22 15:22:54 +1300

    update

--- cbt/service-worker.js
+++ cbt/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.2e7b9aa7a1bf7d572baf1a5570278883.js"
+  "/precache-manifest.17fda9a6f6e67ec0f704c780e433cbce.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
