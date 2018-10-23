8f9934d481d3d4b95f831ea9ce4aa62edb2d6a1b
Glen Davies <glendaviesnz@gmail.com>
2018-10-23 22:14:19 +1300

    Fix paths again

---
+++ cbt/precache-manifest.76a425ab18a40af825592ae24354ef14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "./static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1cdf63e68fcd59c566d4",
+    "url": "./static/js/main.1cdf63e6.chunk.js"
+  },
+  {
+    "revision": "d9527ad1420e0132d9ee",
+    "url": "./static/js/1.d9527ad1.chunk.js"
+  },
+  {
+    "revision": "1cdf63e68fcd59c566d4",
+    "url": "./static/css/main.b034c79e.chunk.css"
+  },
+  {
+    "revision": "098397772124bbc29427f3b624f4c9ac",
+    "url": "./index.html"
+  }
+];
--- cbt/service-worker.js
+++ cbt/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.17fda9a6f6e67ec0f704c780e433cbce.js"
+  "./precache-manifest.76a425ab18a40af825592ae24354ef14.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
